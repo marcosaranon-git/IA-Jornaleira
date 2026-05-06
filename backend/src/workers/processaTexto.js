768a819b651c3c9f56c82e749c8f431f48f1001d
marcosaranon-git <marcosaranon2@gmail.com>
2026-05-05 23:02:52 -0300

    Mudando a IA de tratamento de texto para o 1.5

--- backend/src/workers/processaTexto.js
+++ backend/src/workers/processaTexto.js
@@ -21,7 +21,7 @@ async function processarTextosPendentes() {
         console.log(`🤖 IA Porteira lendo a notícia enviada por: ${nomeNacao}`);
 
         const model = genAI.getGenerativeModel({ 
-            model: "gemini-2.5-flash",
+            model: "gemini-1.5-flash",
             generationConfig: { responseMimeType: "application/json" }
         });
 
